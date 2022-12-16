6025e7c1c4a41aa99a581706535cabe5b727e9fd
sumit kumar <72337263+anudishu@users.noreply.github.com>
2022-12-16 10:54:51 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,6 @@
+output "nat-gateway-id" {
+    value = aws_nat_gateway.nat_gateway_az1
+  
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "public_subnet_az1_id" {}
+variable "internet_gateway_id" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "region" {}
